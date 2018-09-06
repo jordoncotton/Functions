4c8b883d59b70a1e01a7f4f1b699cfecb9a8d8e1
jordoncotton <42358762+jordoncotton@users.noreply.github.com>
2018-09-06 13:12:45 -0500

    Update Source.cpp

--- Functions/Source.cpp
+++ Functions/Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-// Jordon Cotton is awesome and cool
+
 int print(std::string input)
 {
 	std::cout << input;
@@ -33,32 +33,40 @@ void PrintValuesBetween(int low, int high)
 	std::cout << std::endl;
 	system("pause");
 }
-//
+
 //Create a function that will take in a array and the length of the array
 //The function will return the sum of all the values in the array
 int ArraySum(int arr[], int size)
-
 {
-	
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum = arr[i] + sum;
+	}
 }
-
-
-
-
-
-
 
 //create a function that will reverse the values in the array passed in.
 //you will neeed to pass in a array and the size of the array as arguments.
 //cannot use a second array
 void RevArray(int arr[], int size)
 {
+	int rev;
+	for (int i = 0; i < size; i++)
+	{
+		rev = arr[i] + rev;
+	}
 
 }
-
-
 
 int main()
 {
-	ArraySum ()
+	int around[] = { 1, 2, 3, 4 };
+	ArraySum(around, 4);
+
+	int backwards[] = { 4, 3, 2, 1 };
+	RevArray(backwards, 4);
 }
+
+//Homework
+//1 
+
